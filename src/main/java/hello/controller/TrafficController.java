955cf6b8acc09fd8a44939e1a681adac14995adb
bluepeng9 <bluepeng9@gmail.com>
2023-07-01 20:27:16 +0900

    feat: Add jdbc

--- src/main/java/hello/controller/TrafficController.java
+++ src/main/java/hello/controller/TrafficController.java
@@ -1,9 +1,13 @@
 package hello.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,12 +27,24 @@ public class TrafficController {
 
     private List<String> list = new ArrayList<>();
 
+    @Autowired
+    DataSource dataSource;
+
     @GetMapping("/jvm")
     public String jvmc() {
         log.info("jvm");
         for (int i = 0; i < 100000; i++) {
             list.add("hello jvm!" + i);
         }
+        return "ok";
+    }
+
+    @GetMapping("/jdbc")
+    public String jdbc() throws SQLException {
+        log.info("jdbc");
+        Connection conn = dataSource.getConnection();
+        log.info("Connection info={}", conn);
+        //conn.close(); 커넥션을 닫지 않는다.
         return "ok";
     }
 }
