4b47c2cf03149b136806b2882ec7e552c0817b8a
bluepeng9 <bluepeng9@gmail.com>
2023-07-01 20:23:12 +0900

    feat: Add jvm

--- src/main/java/hello/controller/TrafficController.java
+++ src/main/java/hello/controller/TrafficController.java
@@ -4,6 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Slf4j
 @RestController
 public class TrafficController {
@@ -16,5 +19,16 @@ public class TrafficController {
             value++;
         }
         return "ok value=" + value;
+    }
+
+    private List<String> list = new ArrayList<>();
+
+    @GetMapping("/jvm")
+    public String jvmc() {
+        log.info("jvm");
+        for (int i = 0; i < 100000; i++) {
+            list.add("hello jvm!" + i);
+        }
+        return "ok";
     }
 }
